f6edade1c49f7a4e33b044ee23500b999e140f9c
Clara Odebrecht <clara.odebrecht@olist.com>
2021-01-30 20:03:43 -0300

    MODEL: criação de metodo para alterar direcao de submarino

--- backend/model/submarine.py
+++ backend/model/submarine.py
@@ -4,6 +4,21 @@ class Submarine:
         self.latitude = 0
         self.longitude = 0
         self.direction = 'NORTE'
+    
+    def __str__(self) -> None:
+        return f'\n\n({self.latitude}, {self.longitude}, {self.heigth}, {self.direction})'
+
+    def update_direction(self, command: str) -> None:
+        if (self.direction == 'NORTE' and command == 'L') or (self.direction == 'SUL' and command == 'R'):
+            self.direction = 'OESTE'
+        elif (self.direction == 'SUL' and command == 'L') or (self.direction == 'NORTE' and command == 'R'):
+            self.direction = 'LESTE'
+        elif (self.direction == 'LESTE' and command == 'R') or (self.direction == 'OESTE' and command == 'L'):
+            self.direction = 'SUL'
+        elif (self.direction == 'OESTE' and command == 'R') or (self.direction == 'LESTE' and command == 'L'):
+            self.direction = 'NORTE'
+        else:
+            raise ValueError('Invalid command.')
 
     @property
     def heigth(self) -> int:
